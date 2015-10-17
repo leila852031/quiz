b9faf2bfb9bb4cbe23cecaa96c56a94ddb505c73
leila <chenyuyun.leila@gmail.com>
2015-10-18 00:24:14 +0800

    shuffle
    Given an array which contains 1-52. Traversal this array and use rand() to get
     an index which is used to get an element in array. Swap them. Do this many
    times to make sure this array will be mess.

--- q27/main.c
+++ q27/main.c
@@ -1,6 +1,31 @@
-/* FIXME: Implement! */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 52
+void swap(int array[], int i, int j);
 int main()
 {
+    int array[SIZE];   
+    int i,j,a;
+    for(i=0; i<SIZE; i++){
+	array[i]=i+1;
+    }
+    for(j=0;j<1000;j++){
+        for(i=0; i<SIZE; i++){
+   	    a = rand()%52;
+	    swap(array, i, a);
+        }
+    }
+    for(i=0;i<SIZE;i++){
+        printf("%d \n", array[i]);
+    }
     return 0;
 }
+
+void swap(int array[], int i, int j)
+{
+    int tmp=*(array+i);
+    *(array+i)=*(array+j);
+    *(array+j)=tmp;
+}
+
