b9c9f2a8aa345f94856a068aaba8d6a729c5ad56
leila <chenyuyun.leila@gmail.com>
2015-10-17 14:33:37 +0800

    recursive version
    str[] and c is given in main function. In smallest_character function, I give
    some constraints, including c must be an English alphabet and str[] has
    elements.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,28 @@
 /* FIXME: Implement! */
-
+#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+char smallest_character(char str[], char c);
 int main()
 {
+    char test[SIZE]="cfjpv";
+    char ans=smallest_character(test, 'z');
+    printf("%c\n", ans);
     return 0;
+}
+
+char smallest_character(char str[], char c)
+{
+    if((c<'a') || (c>'z')) 
+	return -1;
+    int len = strlen(str);
+    static int i=0;
+    if(len==0) 
+	return -1;
+    if(i==len)
+	return str[0];
+    if(str[i]>c) 
+	return str[i];
+    i++;
+    return smallest_character(str, c);
 }
