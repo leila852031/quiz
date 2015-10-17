1b0b2ab504653781a105bd1c792c0f59efd9b7b4
leila <chenyuyun.leila@gmail.com>
2015-10-17 15:18:10 +0800

    iterative version
    str[] and c is given in main function. In smallest_character function, I give
    some constraints, including c must be an English alphabet and str[] has
    elements.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,26 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+char smallest_character(char str[], char c);
 
 int main()
 {
+    char str[SIZE]="cfjpv";
+    char ans=smallest_character(str, 'c');
+    printf("%c\n", ans);
     return 0;
+}
+
+char smallest_character(char str[], char c)
+{
+    if((c<'a') || (c>'z'))
+        return -1;
+    int len=strlen(str);
+    int i=0;
+    for(i=0; i<len; i++) {
+        if(str[i]>c)
+            return str[i];
+    }
+    return str[0];
 }
