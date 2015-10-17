c4f53c33d4773a0adca85856a6f0d1b947299230
leila <chenyuyun.leila@gmail.com>
2015-10-17 21:10:52 +0800

    recursive version
    Create a binary tree.
    Consider the most simple example.
       1        1     1
      / \  ->  /  ->   \
     2   3    2         2
               \         \
                3         3
    
    step :1. find the most left node
          2. flatten right side of root
          3. connect the root->right to the most right child of root->left
          4. root->right = root->left
          5. root->left = NULL

--- q3/recursive.c
+++ q3/recursive.c
@@ -1,6 +1,86 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 6
+
+struct node{
+    int value;
+    struct node *left;
+    struct node *right;
+};
+typedef struct node TreeNode;
+TreeNode *buildTree (int str[]);
+void insert(TreeNode **node, TreeNode *item);
+TreeNode  *flatten(TreeNode *root);
+void printout(TreeNode *node);
 
 int main()
 {
+    int str[SIZE]={1,2,3,4,5,6};
+    TreeNode *root = (TreeNode *)malloc(sizeof(TreeNode));
+    root = buildTree(str);
+    printout(root);
+    root = flatten(root);
+    printout(root);
     return 0;
 }
+
+TreeNode *buildTree(int str[])
+{   
+    int i;
+    TreeNode *root = (TreeNode *)malloc(sizeof(TreeNode));
+    TreeNode *node;
+    root->value=str[0];
+    for(i=1; i<SIZE; i++){
+	node = (TreeNode *)malloc(sizeof(TreeNode));
+	node->right=NULL;
+	node->left=NULL;
+	node->value=str[i];
+	insert(&root, node);
+    }
+    return root;	
+}
+
+TreeNode *flatten(TreeNode *root)
+{
+    if(root==NULL)
+	return NULL;
+    if(root->left!=NULL){
+	TreeNode *right = root->right;
+	root->right = flatten(root->right);
+	TreeNode *rightMost = root->left;
+	while(rightMost->right) rightMost = rightMost->right;
+	rightMost->right = right;
+	root->right = root->left;
+	root->left=NULL;
+    }
+    return root;
+   
+}
+
+
+
+void insert(TreeNode **node, TreeNode *item){
+    if((*node)==NULL){
+	*node = item;
+	return;
+    }
+    if((item->value)<((*node)->value))
+	insert(&(*node)->left, item);
+    else
+ 	insert(&(*node)->right, item);
+}
+
+
+void printout(TreeNode *node)
+{
+     if(node->left!=NULL)
+	printout(node->left);
+        printf("%d\n", node->value);
+     if(node->right!=NULL)
+	printout(node->right);
+}
+
+
+
